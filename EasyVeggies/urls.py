e79ef57536fd8a038c1dca81ffe01c75d9b46ac8
MartaKasprzyk <martha.kasprzyk@gmail.com>
2024-03-09 22:04:55 +0100

    preparing base view

--- EasyVeggies/urls.py
+++ EasyVeggies/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from GrowVeggies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.BaseView.as_view(), name='base'),
 ]
