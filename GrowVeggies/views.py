3de10804c4c3cc4d1b49b26e840368c5771d4756
MartaKasprzyk <martha.kasprzyk@gmail.com>
2024-03-11 21:35:01 +0100

    created views-post for creation of Seed, Veggie, Company

--- GrowVeggies/views.py
+++ GrowVeggies/views.py
@@ -17,6 +17,15 @@ class VeggieCreateView(LoginRequiredMixin, View):
         form = VeggieCreateForm()
         return render(request, 'form.html', {'form': form})
 
+    def post(self, request):
+        form = VeggieCreateForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            family = form.cleaned_data['family']
+            Veggie.objects.create(name=name, family=family)
+            return redirect('seed_add')
+        return render(request, 'form.html', {'form': form})
+
 
 class CompanyCreateView(LoginRequiredMixin, View):
 
@@ -24,10 +33,30 @@ class CompanyCreateView(LoginRequiredMixin, View):
         form = CompanyCreateForm()
         return render(request, 'form.html', {'form': form})
 
+    def post(self, request):
+        form = CompanyCreateForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            Company.objects.create(name=name)
+            return redirect('seed_add')
+        return render(request, 'form.html', {'form': form})
+
 
 class SeedCreateView(LoginRequiredMixin, View):
 
     def get(self, request):
         form = SeedCreateForm()
+        return render(request, 'seed_add.html', {'form': form})
+
+    def post(self, request):
+        user = request.user
+        form = SeedCreateForm(request.POST)
+        if form.is_valid():
+            veggie = form.cleaned_data['veggie']
+            variety = form.cleaned_data['variety']
+            company = form.cleaned_data['company']
+            comment = form.cleaned_data['comment']
+            Seed.objects.create(owner=user, veggie=veggie, variety=variety, company=company, comment=comment)
+            return redirect('seed_add')
         return render(request, 'seed_add.html', {'form': form})
 
