02db1670fae3e9bc998e814701d09b887936516a
MartaKasprzyk <martha.kasprzyk@gmail.com>
2024-03-12 13:04:45 +0100

    fixed unittest for seed_view_post - pk given in correct way

--- GrowVeggies/tests.py
+++ GrowVeggies/tests.py
@@ -32,7 +32,6 @@ def test_veggie_create_view_get(user):
     assert isinstance(response.context['form'], VeggieCreateForm)
 
 
-@pytest.mark.run(after='test_seed_create_view_post')
 @pytest.mark.django_db
 def test_veggie_create_view_post(user, family):
     client = Client()
@@ -63,7 +62,6 @@ def test_company_create_view_get(user):
     assert isinstance(response.context['form'], CompanyCreateForm)
 
 
-@pytest.mark.run(after='test_veggie_create_view_post')
 @pytest.mark.django_db
 def test_company_create_view_post(user):
     client = Client()
@@ -73,6 +71,7 @@ def test_company_create_view_post(user):
     response = client.post(url, data, follow=True)
     assert response.status_code == 200
     assert Company.objects.get(name='Company_name')
+
 
 @pytest.mark.django_db
 def test_company_update_view_get(user, company):
@@ -93,7 +92,6 @@ def test_seed_create_view_get(user):
     assert isinstance(response.context['form'], SeedCreateForm)
 
 
-@pytest.mark.run(order=1)
 @pytest.mark.django_db
 def test_seed_create_view_post(user, veggie, company):
     client = Client()
@@ -102,8 +100,7 @@ def test_seed_create_view_post(user, veggie, company):
     data = {'veggie': veggie.pk, 'variety': 'variety', 'company': company.pk, 'comment': 'comment'}
     response = client.post(url, data, follow=True)
     assert response.status_code == 200
-    assert Seed.objects.get(owner=user, veggie=1, variety='variety', company=1, comment='comment')
-    # if running single test: veggie,company=1; if running all tests veggie,company=2; preserved info about prev created objs?
+    assert Seed.objects.get(owner=user, veggie=veggie, variety='variety', company=company, comment='comment')
 
 
 @pytest.mark.django_db
