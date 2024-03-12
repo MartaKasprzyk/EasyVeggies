67071e531e1e4ce8287121c09fa6695223ea83a5
MartaKasprzyk <martha.kasprzyk@gmail.com>
2024-03-12 19:18:58 +0100

    added str method to models: Sun/Water/SoilScale, Month

--- GrowVeggies/models.py
+++ GrowVeggies/models.py
@@ -36,17 +36,28 @@ class Month(models.Model):
     name = models.CharField(max_length=20)
     order = models.IntegerField(unique=True)
 
+    def __str__(self):
+        return f'{self.name}'
 
 class SunScale(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class WaterScale(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class SoilScale(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Seed(models.Model):
